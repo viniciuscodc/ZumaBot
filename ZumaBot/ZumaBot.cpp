f98d3f2cae3b5c2762a94b8602fc59d81bf13907
viniciuscodc <viniciuscodc@gmail.com>
2021-10-05 21:49:10 -0300

    Added screen pixel search.

--- ZumaBot/ZumaBot.cpp
+++ ZumaBot/ZumaBot.cpp
@@ -6,18 +6,28 @@
 
 int main()
 {
+    int width = 0;
+    int height = 0;
     bool gameStart{};
+
     while (true) {
         LPCWSTR window_title = L"Zuma Deluxe 1.1.0.0";
         HWND hWnd = FindWindow(NULL, window_title);
-
+        
         if (hWnd != NULL && !gameStart) {
-            hWnd = FindWindow(NULL, window_title);
             std::cout << "Game Started" << std::endl;
-            gameStart = true;
+
+            hWnd = FindWindow(NULL, window_title);
+
             RECT rect = {};
             GetClientRect(hWnd, &rect);
-            std::cout << "bottom: " << rect.bottom << " | " << "right: " << rect.right << std::endl;
+
+            width = rect.right;
+            height = rect.bottom;
+
+            std::cout << "bottom: " << height << " | " << "right: " << width << std::endl;
+            
+            gameStart = true;
         }
 
         Sleep(10);
@@ -25,22 +35,48 @@ int main()
             //HDC hDC = GetDC(hwnd);
             POINT p;
             GetCursorPos(&p);
-            ScreenToClient(hWnd, &p);
+            //ScreenToClient(hWnd, &p);
             std::cout << "x position: " << p.x << " | " << "y position: " << p.y << std::endl;
             Sleep(1000);
         }
-        if (GetAsyncKeyState(VK_NUMPAD2)) {
+
+        if (GetAsyncKeyState(VK_NUMPAD2) && gameStart) {
+            HDC hDC = GetDC(hWnd);
+            COLORREF color;
+            COLORREF baseColor = 8142474;
+
+            printf("Botao pressionado");
+            
+            for (int x = 0; x < height; x++)
+            {
+                for (int y = 0; y < width; y++)
+                {
+                    color = GetPixel(hDC, x, y);
+                    if (color == baseColor) {
+                        printf("Encontrado x: %d | %d:",x,y);
+                        return 0;
+                    }
+                }
+            }
+
+            ReleaseDC(hWnd, hDC);
+            Sleep(1000);
+        }
+
+        if (GetAsyncKeyState(VK_NUMPAD3)) {
             HDC hDC = GetDC(hWnd);
             POINT p;
             GetCursorPos(&p);
             ScreenToClient(hWnd, &p);
             COLORREF color = GetPixel(hDC, p.x, p.y);
             ReleaseDC(hWnd, hDC);
-            std::cout << "Red: " << (int)GetRValue(color) << " | Green: " << (int)GetGValue(color) 
+
+            std::cout << "Color: " << color;
+            /*
+            std::cout << "Red: " << (int)GetRValue(color) << " | Green: " << (int)GetGValue(color)
                 << "| Blue: " << (int)GetBValue(color) << std::endl;
-            Sleep(1000);
-        }
-        if (GetAsyncKeyState(VK_NUMPAD3)) {
+            */
+
             Sleep(1000);
         }
         if (GetAsyncKeyState(VK_NUMPAD4)) {
